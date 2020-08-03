f7924a489b0c17148fda3c3941cc3327960261d4
Knar Gocer <knargr@gmail.com>
2020-08-03 16:17:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch(imgUrl){
+  .
+}
+
+fetch(imgUrl)
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+  for (let i = 0; i< json.length; i++){
+    document.querySelector("dog-image-container").innerHTML=" <img src = json[i]>";
+  }
+})
